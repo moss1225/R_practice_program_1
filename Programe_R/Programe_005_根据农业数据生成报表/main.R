a8d509c538d1b6b5c2ef12aab1785a74092bdf1a
Administrator <Administrator@MOSS>
2017-12-26 13:54:52 +0800

    push me

--- Programe_R/Programe_005_根据农业数据生成报表/main.R
+++ Programe_R/Programe_005_根据农业数据生成报表/main.R
@@ -20,19 +20,59 @@ readDataFunction <- function(fileNames){
 	return(martixData)
 	}
 
+# 读取企业、合作社、个人、其他不同主体类型的函数
+countTypeNum <- function(readDataType1){
+	Type1num <- nrow(readDataType1)
+	# 获取种植业企业数量
+	readDataType11 <- nrow(readDataType1[readDataType1[,6]=="企业" , ])
+	# 获取种植业合作社数量
+	readDataType12 <- nrow(readDataType1[readDataType1[,6]=="合作社" , ])
+	# 获取种植业个人数量
+	readDataType13 <- nrow(readDataType1[readDataType1[,6]=="个人" , ])
+	# 获取种植业其他数量
+	readDataType14 <- nrow(readDataType1[readDataType1[,6]=="其他" , ])
+	# 返回数据，分别是该行业总数、行业企业、行业畜牧、行业个人、行业其他
+	return(c(Type1num , readDataType11 , readDataType12 , readDataType13 , readDataType14))
+} 
+
+# 自动编写文档函数
+writePaper <- function(numAllData , numDataType1 , numDataType2 , numDataType3){
+	return(paste("朝阳区登录在案经确认的主体信息", numAllData , "条，其中种植信息" , numDataType1[1] ,"条，畜牧信息" , numDataType2[1] ,"条，水产" , numDataType3[1]  ,"条，（1）种植信息详细：企业数量" , numDataType1[2] ,"条（约占种植总数%），合作社" , numDataType1[3],"条（约占种植总数%），个人", numDataType1[4] ,"条（约占种植总数%），其他类型", numDataType1[5],"条（约占种植总数%）。（2）畜牧信息详细：企业数量" , numDataType2[2] ,"条（约占种植总数%），合作社" , numDataType2[3] ,"条（约占种植总数%），个人" , numDataType2[4] ,"条（约占种植总数%），其他类型" , numDataType2[5] ,"条（约占种植总数%）。（3）渔业信息详细：企业数量" , numDataType3[2] ,"条（约占种植总数%），合作社" , numDataType3[3] ,"条（约占种植总数%），个人" , numDataType3[4] ,"条（约占种植总数%），其他类型" , numDataType3[5] ,"条（约占种植总数%）。"))
+}
+
 # 第三部分：主函数
 mainFunction <- function(fileNames){
 	# 获取数据
 	readData <<- readDataFunction(fileNames)
+	# 总数据量
+	numAllData <- nrow(readData)
 	# 获取种植业信息
-	readDataType1 <- readData[readData[,4]=="种植业" , 1:4]
+	readDataType1 <- readData[readData[,4]=="种植业" , ]
 	# 获取种植业信息数量 
-	Type1num <- nrow(readDataType1)
+	numDataType1 <- countTypeNum(readDataType1)
+	# 获取种植业信息
+	readDataType2 <- readData[readData[,4]=="畜牧业" , ]
+	# 获取种植业信息数量 
+	numDataType2 <- countTypeNum(readDataType2)
+	# 获取种植业信息
+	readDataType3 <- readData[readData[,4]=="渔业" , ]
+	# 获取种植业信息数量 
+	numDataType3 <- countTypeNum(readDataType3)
+	# 调用编写文档
+	# print(writePaper(numAllData , numDataType1 , numDataType2 , numDataType3))
+	print(numAllData)
+	print(numDataType1[c(2:5,1)])
+	print(numDataType2[c(2:5,1)])
+	print(numDataType3[c(2:5,1)])
+	print(paste("企业" , numDataType1[2]+numDataType2[2]+numDataType3[2]))
+	print(paste("合作" , numDataType1[3]+numDataType2[3]+numDataType3[3]))
+	print(paste("个人" , numDataType1[4]+numDataType2[4]+numDataType3[4]))
+	print(paste("其他" , numDataType1[5]+numDataType2[5]+numDataType3[5]))
 } 
 
 # 第四部分部分：通过对话框获取文件名称参数
 # fileName <- readline("Please inpute your files name :")
-fileName <-  "朝阳区.csv"
+fileName <-  "怀柔区.csv"
 # 备选：fileNames <- scan()
 		
 # 第五部分：执行主函数
---
+++ Programe_R/Programe_006_主体数据核查分析/Main.R
@@ -0,0 +1,35 @@
+dirPath <-"C:\\Users\\Administrator\\git\\R_practice_program_1\\Programe_R\\Programe_006_主体数据核查分析\\data"
+setwd(dirPath)
+fileNames="agri.csv"
+readData <- read.table(fileNames , header = TRUE , sep =",")
+mainData <- data.frame(readData)
+
+# 企业、合作社、个人序号
+num1 <- which(mainData[,2]=="企业")
+num2 <- which(mainData[,2]=="合作社")
+num3 <- which(mainData[,2]=="个人")
+
+# 企业、合作社、个人调查总数
+sum1 <- sum(mainData[num1 ,  c(3 , 4 , 5)] ) # 企业
+sum2 <- sum(mainData[num2 ,  c(3 , 4 , 5)] ) # 合作社
+sum3 <- sum(mainData[num3 ,  c(3 , 4 , 5)] ) # 个人
+
+# 使用factor()函数将地区属性因子化
+factorDistrict <- factor(mainData[ , 1])
+# 获取地区属性因子水平
+levDistrict <- c( levels(factorDistrict) )
+
+# 各城区调查总数
+# 创建地区统计结果集
+District=c("地区")
+sumNum=c("数量")
+DistrictData <- data.frame(District , sumNum , stringsAsFactors =F)
+# 通过循环计算和返回该地区总数
+for ( i in levDistrict){
+	sumDistrict <- sum(mainData[which(mainData==i) , c(3 , 4 , 5)])
+    # print( paste(i , sumDistrict))
+	# 将结果返回到结果集合
+	addDistrict <- data.frame(District=c(i) , sumNum=c(sumDistrict) , stringsAsFactors =F)
+	print(addDistrict)
+	DistrictData <- data.frame(District=c(DistrictData$District , addDistrict$District) , sumNum=c(DistrictData$sumNum , addDistrict$sumNum) , stringsAsFactors =F)
+}
