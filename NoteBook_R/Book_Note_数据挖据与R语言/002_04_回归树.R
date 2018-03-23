6d7049f30a34ac6b83c71d27be1d87fe3f7b78dc
Administrator <Administrator@MOSS>
2018-03-23 17:58:36 +0800

    push me

--- NoteBook_R/Book_Note_数据挖据与R语言/002_04_回归树.R
+++ NoteBook_R/Book_Note_数据挖据与R语言/002_04_回归树.R
@@ -3,6 +3,15 @@
 # 建立回归树模型代码如下：
 # 引入rpart包，引入回归模型
 library(rpart)
+library(DMwR)
 data(algae)
-algae <- algae[-mangyNAs(algae) , ]
+algae <- algae[-manyNAs(algae) , ]
 rt.a1 <- rpart( a1 ~ . , data = algae[ , 1:12])
+first.tree <- rt.a1
+prettyTree(rt.a1)
+# 使用rpart()函数构建书，在构建树的过程中，当给定条件满足时构建过程就停止。
+# 1）偏差的减少小于摸一个给定限制时；
+# 2）当结点中的样本数量小于某个给定界限时；
+# 3）当树的深度大于一个给定界限值。
+first.tree <- snip.rpart(first.tree )
+prettyTree(first.tree)
